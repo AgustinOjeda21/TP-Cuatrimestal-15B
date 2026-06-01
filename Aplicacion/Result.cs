7731a71fc11e9659bbf0355a540ac7e805626913
AgustinOjeda21 <agustinojeda398@gmail.com>
2026-06-01 01:31:52 -0300

    Comienzo de modelado de gestores

---
+++ Aplicacion/Result.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services.Description;
+
+namespace Aplicacion
+{
+    public static class Result<T>
+    {
+        public static bool Success { get; set; }
+        public static string Message { get; set; }
+        public static T Value { get; set; }
+
+        public Result<T>(bool success, string message, T value)
+        {
+            Success = Success;
+            Message = Message;
+            Value = Value;
+        }
+
+        public static void EjecucionCorrecta(this Result<T> result)
+        {
+            result.Success = true;
+            Message = "Ejecucion Correcta";
+            Value = default;
+            return result
+        }
+        public static void Ok(T value)
+        {
+            Success = true;
+            Message = "";
+            Value = value;
+        }
+
+        public static void Fail(string message)
+        {
+            Success = false;
+            Message = message;
+            Value = default;
+        }
+    }
+}
